28f446b2e59ba58fb31b89379ec0ff29ed4a29ae
Anton Prokopov <prokopov.anton@gmail.com>
2024-03-04 15:14:06 +0000

    ANTON-0001: added dependenciews for Hilt

--- app/src/main/java/com/prokopov/hometask/ui/SplashActivity.kt
+++ app/src/main/java/com/prokopov/hometask/ui/SplashActivity.kt
@@ -1,11 +1,13 @@
-package com.prokopov.hometask
+package com.prokopov.hometask.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.prokopov.hometask.R
 
-class MainActivity : AppCompatActivity() {
+
+class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_splash)
     }
 }
